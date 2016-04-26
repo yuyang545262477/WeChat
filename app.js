b65333254931fe1dccb330673c345c56b579d219
David_Yu <yuyang545262477@gmail.com>
2016-04-26 14:11:50 +0800

    通过验证

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var Koa = require('koa');
 var sha1 = require('sha1');
 var config = {
     wechat: {
-        appID: 'wx4899993eb525d224',
-        appSecret: 'bf6a554e226a37380779aecbed719233',
+        appID: 'wxa41198eb824d5209',
+        appSecret: '6dabafc584ea2af75a148e956cb206de',
         token: 'yuxiaoyang'
     }
 };
@@ -16,6 +16,24 @@ var app = new Koa();
 
 app.use(function *(next) {
     console.log(this.query);
+    // get somthing
+    var token = config.wechat.token,
+        signature = this.query.signature,
+        nonce = this.query.nonce,
+        timestamp = this.query.timestamp,
+        echostr = this.query.echostr;
+    //  deal with sthing.
+
+    var str = [token, timestamp, nonce].sort().join(''),
+        sha = sha1(str);
+
+    if (sha == signature) {
+        this.body = echostr + '';
+    } else {
+        this.body = 'wrong';
+    }
+
+
 });
 
 app.listen(8080);
